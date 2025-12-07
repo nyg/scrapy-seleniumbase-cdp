86271da447b3dde1d5306bcfd67dc731a4be36f0
nyg <nyg@users.noreply.github.com>
2025-12-07 14:42:32 +0100

    core: fix circular import for SeleniumBaseRequest

--- scrapy_seleniumbase_cdp/middleware_async.py
+++ scrapy_seleniumbase_cdp/middleware_async.py
@@ -17,7 +17,7 @@ from importlib import import_module
 from scrapy import signals
 from scrapy.http import HtmlResponse
 
-from scrapy_seleniumbase_cdp import SeleniumBaseRequest
+from .request import SeleniumBaseRequest
 
 
 class SeleniumBaseAsyncCDPMiddleware:
