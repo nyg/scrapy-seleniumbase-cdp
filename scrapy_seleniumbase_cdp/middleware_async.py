1d290760ca93f3f38a7b8663318e7d59be9f6648
nyg <nyg@users.noreply.github.com>
2025-12-27 13:04:53 +0100

    refactor!: add error handling decorators and improve middleware API

--- scrapy_seleniumbase_cdp/middleware_async.py
+++ scrapy_seleniumbase_cdp/middleware_async.py
@@ -1,8 +1,10 @@
 from base64 import b64decode
+from functools import wraps
+from typing import Any
 
 import mycdp
-from scrapy import Request
-from scrapy import signals, Spider
+from scrapy import Request, Spider, signals
+from scrapy.crawler import Crawler
 from scrapy.http import HtmlResponse
 from seleniumbase.undetected import cdp_driver
 from seleniumbase.undetected.cdp_driver.browser import Browser
@@ -11,27 +13,41 @@ from seleniumbase.undetected.cdp_driver.tab import Tab
 from .request import SeleniumBaseRequest
 
 
+def handle_errors(error_msg: str):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                spider = [arg for arg in args if isinstance(arg, Spider)][0]
+                spider.logger.exception(f'{error_msg}: {e}')
+
+        return wrapper
+
+    return decorator
+
+
 class SeleniumBaseAsyncCDPMiddleware:
     """
     Scrapy downloader middleware handling the requests using SeleniumBase pure CDP mode instead of the UC mode.
     Uses SeleniumBase's async/await API because the event loop of the pure CDP mode conflicts with Scrapy's own event loop.
     """
 
-    def __init__(self, browser_options: dict[str, str]):
+    def __init__(self, browser_options: dict[str, Any]):
         """Initialize the middleware.
 
         Args:
-            browser_options (dict[str, str]): A dictionary of keyword arguments to initialize the
+            browser_options (dict[str, Any]): A dictionary of keyword arguments to initialize the
                 SeleniumBrowser with.
         """
         self.browser: Browser | None = None
         self.browser_options = browser_options
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler):
         """Initialize the middleware with the crawler settings."""
-        browser_options = crawler.settings.get('SELENIUMBASE_BROWSER_OPTIONS', {})
-        middleware = cls(browser_options)
+        middleware = cls(crawler.settings.get('SELENIUMBASE_BROWSER_OPTIONS', {}))
         crawler.signals.connect(middleware.spider_opened, signals.spider_opened)
         crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
         return middleware
@@ -62,57 +78,49 @@ class SeleniumBaseAsyncCDPMiddleware:
                             headers={'Cookie': '; '.join(cookies)})
 
     @staticmethod
+    @handle_errors("Error waiting for element")
     async def _wait_for_element(tab: Tab, request: SeleniumBaseRequest, spider: Spider):
         """Wait for the specified element if requested."""
         if not request.wait_for:
             return
 
-        try:
-            await tab.wait_for(selector=request.wait_for, timeout=getattr(request, 'wait_timeout', 10))
-        except Exception as e:
-            spider.logger.warning(f'Element not found: {request.wait_for}, {e}')
+        await tab.wait_for(selector=request.wait_for, timeout=request.wait_timeout)
 
+    @handle_errors("Error executing browser callback")
     async def _execute_callback(self, request: SeleniumBaseRequest, spider: Spider):
         """Execute the browser callback method if requested."""
         if not request.browser_callback:
             return
 
-        try:
-            request.meta['callback'] = await request.browser_callback(self.browser)
-        except Exception as e:
-            spider.logger.warning(f'Error executing browser callback: {e}')
+        request.meta['callback'] = await request.browser_callback(self.browser)
 
     @staticmethod
+    @handle_errors("Error executing script")
     async def _execute_script(tab: Tab, request: SeleniumBaseRequest, spider: Spider):
         """Execute the JavaScript code if requested."""
-        if not request.script:
+        if not request.script or not request.script.get('script'):
             return
 
-        try:
-            request.meta['script'] = await tab.evaluate(request.script.get('script', ''),
-                                                        await_promise=request.script.get('await_promise', False))
-        except Exception as e:
-            spider.logger.warning(f'Error executing script: {e}')
+        request.meta['script'] = await tab.evaluate(request.script['script'],
+                                                    await_promise=request.script.get('await_promise', False))
 
     @staticmethod
+    @handle_errors("Error taking screenshot")
     async def _take_screenshot(tab: Tab, request: SeleniumBaseRequest, spider: Spider):
         """Take a screenshot if requested."""
-        if request.screenshot in (None, False):
+        if not request.screenshot:
             return
 
-        try:
-            image_format = request.screenshot.get('format', 'png')
-            full_page = request.screenshot.get('full_page', True)
+        image_format = request.screenshot.get('format', 'png')
+        full_page = request.screenshot.get('full_page', True)
 
-            if request.screenshot.get('path'):
-                path = await tab.save_screenshot(request.screenshot.get('path'), image_format, full_page)
-                spider.logger.info(f'Screenshot saved in {path}')
-            else:
-                command = mycdp.page.capture_screenshot(format_=image_format, capture_beyond_viewport=full_page)
-                request.meta['screenshot'] = b64decode(await tab.send(command))
-                spider.logger.info('Screenshot saved in response.meta["screenshot"]')
-        except Exception as e:
-            spider.logger.warning(f'Screenshot could not be saved: {e}')
+        if request.screenshot.get('path'):
+            path = await tab.save_screenshot(request.screenshot.get('path'), image_format, full_page)
+            spider.logger.info(f'Screenshot saved in {path}')
+        else:
+            command = mycdp.page.capture_screenshot(format_=image_format, capture_beyond_viewport=full_page)
+            request.meta['screenshot'] = b64decode(await tab.send(command))
+            spider.logger.info('Screenshot saved in response.meta["screenshot"]')
 
     async def spider_opened(self, spider):
         """Start the CDP browser when the spider opens."""
